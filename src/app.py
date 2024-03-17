3b5e4b090cb710938aa2db9dea97dc0a37a47f62
DianaJRoo <149723555+DianaJRoo@users.noreply.github.com>
2024-03-17 23:45:53 +0000

    TodosApiListo

--- src/app.py
+++ src/app.py
@@ -8,19 +8,30 @@ def hello_world():
     return jsonify(todos)
 
 todos = [
-    { 
+     { 
        "label": "My first task", 
        "done": False 
-       },
+     },
+
+     { 
+       "label": "My first task 1", 
+       "done": False 
+     },
 ]
 
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
     request_body = request.json
     print("Incoming request with the following body", request_body)
-    return 'Response for the POST todo'
+    todos.append(request_body)
+    return jsonify(todos)
 
-
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    new_deleted_todo = todos[position]
+    todos.remove(new_deleted_todo)
+    print("This is the position to delete:", position)
+    return jsonify(todos)
 
 
 if __name__ == '__main__':
